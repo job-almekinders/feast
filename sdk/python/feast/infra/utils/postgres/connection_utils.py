a29d25c01415d7f62c30957f66f2bac36a98d744
Job Almekinders <job.almekinders@teampicnic.com>
2024-06-24 15:13:17 +0200

    Utils: Add _get_connection_pool_async
    Signed-off-by: Job Almekinders <job.almekinders@teampicnic.com>

--- sdk/python/feast/infra/utils/postgres/connection_utils.py
+++ sdk/python/feast/infra/utils/postgres/connection_utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 import psycopg
 import pyarrow as pa
 from psycopg.connection import Connection
-from psycopg_pool import ConnectionPool
+from psycopg_pool import AsyncConnectionPool, ConnectionPool
 
 from feast.infra.utils.postgres.postgres_config import PostgreSQLConfig
 from feast.type_map import arrow_to_pg_type
@@ -24,6 +24,17 @@ def _get_conn(config: PostgreSQLConfig) -> Connection:
 def _get_connection_pool(config: PostgreSQLConfig) -> ConnectionPool:
     """Get a psycopg `ConnectionPool`."""
     return ConnectionPool(
+        conninfo=_get_conninfo(config),
+        min_size=config.min_conn,
+        max_size=config.max_conn,
+        open=False,
+        kwargs=_get_conn_kwargs(config),
+    )
+
+
+async def _get_connection_pool_async(config: PostgreSQLConfig) -> AsyncConnectionPool:
+    """Get a psycopg `AsyncConnectionPool`."""
+    return AsyncConnectionPool(
         conninfo=_get_conninfo(config),
         min_size=config.min_conn,
         max_size=config.max_conn,
